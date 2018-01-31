d2ceb8a8e4515b34b65a1ba712f9f4ce7f96ba27
LeoDog896 <LeoDog896@hotmail.com>
2018-01-31 14:32:01 -0500

    Update example.js

--- example.js
+++ example.js
@@ -1,23 +1,24 @@
 var engine = new JavaSkriptEngine();
-engine.engine.add('alert', function (args) {
+var system = engine.engine;
+system.add('alert', function (args) {
     if (['alert the message ', 'alert a message ', 'alert message ', 'alert '].includes(args[0])) {
         if ([' to the player', ''].includes(args[2])) {
             alert(args[1]);
         } else {
-            engine.engine.error('Argument 3 is invalid');
+            system.error('Argument 3 is invalid');
         }
     } else {
-        engine.engine.error('Argument 1 is invalid');
+        system.error('Argument 1 is invalid');
     }
 });
-engine.engine.add('log', function (args) {
+system.add('log', function (args) {
     if (['log the message ', 'log a message ', 'log message ', 'log '].includes(args[0])) {
         if ([' to the console', ''].includes(args[2])) {
             console.log(args[1]);
         } else {
-            engine.engine.error('Argument 3 is invalid');
+            system.error('Argument 3 is invalid');
         }
     } else {
-        engine.engine.error('Argument 1 is invalid');
+        system.error('Argument 1 is invalid');
     }
 });
